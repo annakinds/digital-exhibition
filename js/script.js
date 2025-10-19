f0ac289dc6125037e3b1c7869802c2c6dc88a9e4
Anna Kinds <anna.kinds@student.howest.be>
2025-10-19 15:43:14 +0200

    back to import shaders

--- js/script.js
+++ js/script.js
@@ -1,12 +1,7 @@
 import * as THREE from "https://unpkg.com/three@0.169.0/build/three.module.js";
 import { GLTFLoader } from "https://unpkg.com/three@0.169.0/examples/jsm/loaders/GLTFLoader.js";
-const loadShader = async (url) => {
-    const response = await fetch(url);
-    return await response.text();
-}
-
-const portalVertexShader = await loadShader('../shaders/portal/vertex.glsl?raw');
-const portalFragmentShader = await loadShader('../shaders/portal/fragment.glsl?raw');
+import portalVertexShader from '../shaders/portal/vertex.glsl?raw';
+import portalFragmentShader from '../shaders/portal/fragment.glsl?raw';
 
 const canvas = document.querySelector('canvas.webgl');
 const scene = new THREE.Scene();
